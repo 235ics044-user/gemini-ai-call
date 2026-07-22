df16cbbb28e90c786927df8b9554aba16e343282
Rudraksh Gupta <decodewithyash9953@gmail.com>
2026-07-22 22:09:53 +0530

    Integrate Google Gemini API into main function
    Refactor main function to use Google Gemini API for content generation instead of Appwrite Users service.

--- src/main.py
+++ src/main.py
@@ -1,39 +1,25 @@
-from appwrite.client import Client
-from appwrite.services.users import Users
-from appwrite.exception import AppwriteException
 import os
+from google import genai
 
-# This Appwrite function will be executed every time your function is triggered
+
 def main(context):
-    # You can use the Appwrite SDK to interact with other services
-    # For this example, we're using the Users service
-    client = (
-        Client()
-        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
-        .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-        .set_key(context.req.headers["x-appwrite-key"])
-    )
-    users = Users(client)
-
     try:
-        response = users.list()
-        # Log messages and errors to the Appwrite Console
-        # These logs won't be seen by your end users
-        context.log("Total users: " + str(response["total"]))
-    except AppwriteException as err:
-        context.error("Could not list users: " + repr(err))
+        client = genai.Client(
+            api_key=os.environ["GEMINI_API_KEY"]
+        )
 
-    # The req object contains the request data
-    if context.req.path == "/ping":
-        # Use res object to respond with text(), json(), or binary()
-        # Don't forget to return a response!
-        return context.res.text("Pong")
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents="Hello Gemini!"
+        )
 
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
-    )
+        return context.res.json({
+            "success": True,
+            "response": response.text
+        })
+
+    except Exception as e:
+        return context.res.json({
+            "success": False,
+            "error": str(e)
+        }, 500)
